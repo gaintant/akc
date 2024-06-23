57b9a6185e7554c024388c645c753971b5f11a66
Siddhesh Kothadi <realsiddheshko@gmail.com>
2024-06-23 22:17:36 +0530

    Adds standalone config and Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
-
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  output: "standalone",
+};
 
 export default config;
