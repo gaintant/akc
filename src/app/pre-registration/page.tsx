859f447d35cd1e0914bd3184e538f0ad13226949
ravisha7feb <ravisha7feb@gmail.com>
2024-06-25 01:15:51 +0530

    Pre Registration Form Added

--- src/app/pre-registration/page.tsx
+++ src/app/pre-registration/page.tsx
@@ -2,9 +2,22 @@ import InputField from "../(components)/InputFields/PlainTextInput"
 
 export default function Page() {
     return (
-    <div className="self-center">
-        <div>
-            <InputField labelName="School Name"/>
+    <div className="m-6 w-5/12">
+        <div className="font-semibold text-black text-lg m-5">Pre Registration</div>
+        <div className="flex justify-between">
+            <InputField labelName="School ID" isRequired={true} />
+            <InputField labelName="School Name" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="Street Address" isRequired={true} />
+            <InputField labelName="City" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="State" isRequired={true} />
+            <InputField labelName="Pincode" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="School Website" isRequired={false} />
         </div>
     </div>
     )
