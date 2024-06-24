23e81a21ee3704c6da8f326314395c67550148ab
ravisha7feb <ravisha7feb@gmail.com>
2024-06-24 20:36:56 +0530

    Input fields added

---
+++ src/app/pre-registration/page.tsx
@@ -0,0 +1,9 @@
+import InputField from "../(components)/InputFields/PlainTextInput"
+
+export default function Page() {
+    return (
+    <div className="self-center">
+        <InputField />
+    </div>
+    )
+  }
