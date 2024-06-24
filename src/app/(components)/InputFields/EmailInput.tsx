80a64efd918f055e7831cd5f34147402d0a9595f
ravisha7feb <ravisha7feb@gmail.com>
2024-06-25 01:15:51 +0530

    Pre Registration Form Added

--- src/app/(components)/InputFields/EmailInput.tsx
+++ src/app/(components)/InputFields/EmailInput.tsx
@@ -1,10 +1,14 @@
-export default function InputField () {
+export default function EmailInput (props: any) {
 
     return (
         <div className="self-center">
-            <label className="block text-xl leading-6 text-gray-900 mb-1">
+            <label className="block text-base leading-6 text-gray-900 mb-1">
                 Email Address
+                {props.isRequired &&
+                    <div className="inline-block"> * </div>
+                }
             </label>
+
             <input type="email" placeholder="text" className="border-gray-200 border-4 h-10 rounded-md
              placeholder-gray-400 p-2">
             </input>
--- src/app/pre-registration/page.tsx
+++ src/app/pre-registration/page.tsx
@@ -1,11 +1,39 @@
 import InputField from "../(components)/InputFields/PlainTextInput"
+import EmailInput from "../(components)/InputFields/EmailInput"
 
 export default function Page() {
     return (
-    <div className="self-center">
-        <div>
-            <InputField labelName="School Name"/>
+    <div className="m-6 w-5/12 p-5">
+        <div className="font-semibold text-black text-lg">Pre Registration</div>
+        <div className="flex justify-between">
+            <InputField labelName="School ID" isRequired={true} />
+            <InputField labelName="School Name" isRequired={true} />
         </div>
+        <div className="flex justify-between">
+            <InputField labelName="Street Address" isRequired={true} />
+            <InputField labelName="City" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="State" isRequired={true} />
+            <InputField labelName="Pincode" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="School Website" isRequired={false} />
+        </div>
+
+        <div className="font-semibold text-black text-lg">Contact</div>
+        <div className="flex justify-between">
+            <InputField labelName="First Name" isRequired={true} />
+            <InputField labelName="Surname" isRequired={true} />
+        </div>
+        <div className="flex justify-between">
+            <InputField labelName="Daylight Telephone number" isRequired={true} />
+            <EmailInput isRequired={true} />
+        </div>
+
+        <div> * mandatory field </div>
+        {/* create a button component also with text prop */}
+        <button className="bg-red-900 w-40 h-12 text-slate-50 rounded-sm m-5">Submit</button>
     </div>
     )
   }
