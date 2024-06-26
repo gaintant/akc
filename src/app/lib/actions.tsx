0ff2aec49eecfba27281072985be6d049faeccf3
ravisha7feb <ravisha7feb@gmail.com>
2024-06-26 18:57:20 +0530

    Add Data Type Validator for Pre Registration Form

--- src/app/lib/actions.tsx
+++ src/app/lib/actions.tsx
@@ -1,6 +1,24 @@
 'use server';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    SchoolID: z.string(),
+    SchoolName: z.string(),
+    StreetAddress: z.string(),
+    City: z.string(),
+    State: z.string(),
+    Pincode: z.coerce.number(),
+    SchoolWebsite: z.string(),
+    FirstName: z.string(),
+    Surname: z.string(),
+    DaylightTelephonenumber: z.coerce.number(),
+    date: z.string(),
+})
 
 export async function createRegistrationData(formData: FormData) {
-    const rawFormData =  Object.fromEntries(formData.entries())
-    console.log(rawFormData)
+    const CreateInvoice = FormSchema.omit({ id: true, date: true });
+    const data = CreateInvoice.parse(Object.fromEntries(formData.entries()))
+    console.log(data)
+    // console.log(typeof data.DaylightTelephonenumber)
 }
