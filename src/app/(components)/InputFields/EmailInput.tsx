398586da0bb9690aef5558899350c66f896aabe8
Siddhesh Kothadi <realsiddheshko@gmail.com>
2024-06-26 22:03:44 +0530

    Fix build

--- src/app/(components)/InputFields/EmailInput.tsx
+++ src/app/(components)/InputFields/EmailInput.tsx
@@ -1,18 +1,25 @@
-export default function EmailInput (props: any) {
+import React from "react";
 
-    return (
-        <div className="self-center">
-            <label className="block text-base leading-6 text-gray-900 mb-1">
-                Email Address
-                {props.isRequired &&
-                    <div className="inline-block"> * </div>
-                }
-            </label>
-
-            <input type="email" placeholder="text" className="border-gray-200 border-4 h-10 rounded-md
-             placeholder-gray-400 p-2">
-            </input>
-        </div>
-    )
-
+interface IEmailInput {
+  isRequired?: boolean;
 }
+
+const EmailInput: React.FC<IEmailInput> = (props: IEmailInput) => {
+  return (
+    <div className="self-center">
+      <label className="mb-1 block text-base leading-6 text-gray-900">
+        Email Address
+        {props.isRequired && <div className="inline-block"> * </div>}
+      </label>
+
+      <input
+        type="email"
+        placeholder="text"
+        className="h-10 rounded-md border-4 border-gray-200
+             p-2 placeholder-gray-400"
+      ></input>
+    </div>
+  );
+};
+
+export default EmailInput;
--- src/app/(components)/InputFields/PlainTextInput.tsx
+++ src/app/(components)/InputFields/PlainTextInput.tsx
@@ -1,18 +1,25 @@
-export default function InputField (props: any) {
-    console.log(props)
-    return (
-        <div className="self-center m-5">
-            <label className="block text-base leading-6 text-gray-900 mb-1">
-                {props.labelName}
-                
-                {props.isRequired &&
-                    <div className="inline-block"> * </div>
-                }
-            </label>
-            <input placeholder="text" className="border-gray-200 border-4 h-10 rounded-md
-             placeholder-gray-400 p-2">
-            </input>
-        </div>
-    )
+import React from "react";
 
+interface IInputField {
+  labelName?: string;
+  isRequired?: boolean;
 }
+
+const InputField: React.FC<IInputField> = (props) => {
+  return (
+    <div className="m-5 self-center">
+      <label className="mb-1 block text-base leading-6 text-gray-900">
+        {props.labelName}
+
+        {props.isRequired && <div className="inline-block"> * </div>}
+      </label>
+      <input
+        placeholder="text"
+        className="h-10 rounded-md border-4 border-gray-200
+             p-2 placeholder-gray-400"
+      ></input>
+    </div>
+  );
+};
+
+export default InputField;
