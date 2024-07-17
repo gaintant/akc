bb4d4e5dfdfaf39dc13522333b6b3f424169ff9f
SureshKamediya <sureshkamediya18@gmail.com>
2024-07-18 01:14:43 +0530

    Complete how-it-works page design and content

---
+++ src/app/(components)/ExpandableCard.tsx
@@ -0,0 +1,86 @@
+interface ExpandableCardProps {
+  heading: string;
+  content: string;
+  isExpanded: boolean;
+  onClick: () => void;
+}
+
+const ExpandableCard: React.FC<ExpandableCardProps> = ({
+  heading,
+  content,
+  isExpanded,
+  onClick,
+}) => {
+  return (
+    <div
+      className={
+        isExpanded
+          ? `relative mb-4 w-full rounded-lg border border-black bg-white p-4 shadow-[0_4px_6px_rgba(0,0,0,0.8),0_1px_3px_rgba(0,0,0,0.08)] md:max-w-[60%]`
+          : `relative mb-4 w-full rounded-lg border border-black p-4 md:max-w-[60%]`
+      }
+    >
+      <div className="flex items-center">
+        <button
+          onClick={onClick}
+          className="flex h-8 w-8 transform items-center justify-center rounded-full bg-blue-500 text-white transition-transform duration-200"
+        >
+          {isExpanded ? (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path d="M17 15.586 6.707 5.293 5.293 6.707 15.586 17H7v2h12V7h-2v8.586z" />
+            </svg>
+          )}
+        </button>
+        <h2 className="ml-4 text-xl font-bold">{heading}</h2>
+      </div>
+      {isExpanded && heading == "Requirements of an Athelectics Kids Cup" ? (
+        <div className="ml-12 mt-4 font-semibold">
+          <ol className="list-inside list-decimal">
+            <li>Registration of the school sports day on our website</li>
+            <li>Wearing the starting numbers supplied free of charge</li>
+            <li>
+              Reporting of the results using the free evaluation software
+              provided
+            </li>
+          </ol>
+          <br></br>
+          {content}
+        </div>
+      ) : isExpanded &&
+        heading == "Advantages of organising an Atheletics Kids Cup" ? (
+        <div className="ml-12 mt-4 font-semibold">
+          {content}
+          <br />
+          <br />
+          <ul className="list-inside list-disc">
+            <li>
+              A ready-to-use sport's day concept (including various ways of
+              orgainising the event)
+            </li>
+            <li>
+              Simple evaluation software that enables you to capture results and
+              analyse them in ranking lists.
+            </li>
+            <li>Free result sheets to simplify the organisational process.</li>
+            <li>
+              Free orgainsational material for running the sports day(measuring
+              taps, balls, stopwatches, etc.)
+            </li>
+            <li>
+              You will receive bib numbers and participant certificates free of
+              charge.
+            </li>
+            <li>Data protection is guranteed at all times.</li>
+          </ul>
+        </div>
+      ) : (
+        isExpanded && <div className="ml-12 mt-4 font-semibold">{content}</div>
+      )}
+    </div>
+  );
+};
+
+export default ExpandableCard;
--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import ExpandableCard from "../(components)/ExpandableCard";
 
 const HowItWorks = () => {
+  const [expandedCard, setExpandedCard] = useState<number | null>(1);
+
+  const cards = [
+    {
+      id: 1,
+      heading: "Overview",
+      content:
+        "The Athletics Kids Cup is a major youth project in India supported by UBS. " +
+        "The children and adolescents between the ages of 7 and 15 will all compete in " +
+        "the same three events: 60m sprint, long jump and ball throw. We recommend that you " +
+        "orgainize the Atheletics Kids Cup as your atheletics sports day, as there are only three " +
+        "requirements to met and you can benefit in many ways.",
+    },
+    {
+      id: 2,
+      heading: "Requirements of an Athelectics Kids Cup",
+      content:
+        "It's as simple as that. You can now express your interest in becoming part of the " +
+        "Athletics Kids Cup movement by pre-registering your school. We will then inform you " +
+        "as soon as registration for an event organised by you is possible.",
+    },
+    {
+      id: 3,
+      heading: "Advantages of organising an Atheletics Kids Cup",
+      content:
+        "We support you in orgainising an Atheletics Kids Cup. Each school receives a ready-to-use toolkit " +
+        "to orgainse its own competition. The toolkit includes",
+    },
+    {
+      id: 4,
+      heading: "Regional Finals",
+      content:
+        "Numerous Atheletics Kids Cup competitions will be held throughout Mumbai from mid September 2024. " +
+        "The best atheletes will be invited to one of the five regioinal finals in Januray 2025",
+    },
+    {
+      id: 5,
+      heading: "Grand Mumbai Final",
+      content:
+        "The Atheletics Kids Cup Grand Mumbai Final will take place on 15 February 2025 at the Jio Institute's " +
+        "Atheletic Center in Navi Mumbai. In which the best girls and boys in each age category can participate.",
+    },
+  ];
+
   return (
-    <div>HowItWorks</div>
-  )
-}
+    <div className="min-h-screen bg-green-100 md:bg-white">
+      <div className="p-6 text-center font-sans text-3xl font-bold underline underline-offset-2">
+        How it works
+      </div>
+      <div className="relative flex min-h-screen flex-col md:flex-row">
+        <div className="w-full bg-green-500 p-4 md:w-[80%] md:rounded-r-[30%]">
+          <div className="flex flex-col items-start md:ml-8">
+            {cards.map((card) => (
+              <ExpandableCard
+                key={card.id}
+                heading={card.heading}
+                content={card.content}
+                isExpanded={expandedCard === card.id}
+                onClick={() =>
+                  setExpandedCard(expandedCard === card.id ? null : card.id)
+                }
+              />
+            ))}
+          </div>
+        </div>
+        <div className="relative flex w-full justify-center bg-white md:h-screen md:w-[30%] md:bg-transparent">
+          <img
+            src="/images/how-it-works.jpeg"
+            alt="Right Image"
+            className="oval-image h-auto max-h-[600px] w-3/4 max-w-[400px] object-cover md:absolute md:left-[-50%] md:top-1/2 md:w-[500px] md:-translate-y-1/2 md:transform lg:w-[600px] xl:w-[700px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HowItWorks;
