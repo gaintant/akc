98469ae7091f4c0a1a3191f91cb4584500af7d33
SureshKamediya <sureshkamediya18@gmail.com>
2024-07-18 22:05:59 +0530

    Changed image and cards order and fixed bullet points view

--- src/app/(components)/ExpandableCard.tsx
+++ src/app/(components)/ExpandableCard.tsx
@@ -11,6 +11,15 @@ const ExpandableCard: React.FC<ExpandableCardProps> = ({
   isExpanded,
   onClick,
 }) => {
+  const bulletPoints = [
+    "A ready-to-use sport's day concept (including various ways of orgainising the event)",
+    "Simple evaluation software that enables you to capture results and analyse them in ranking lists.",
+    "Free result sheets to simplify the organisational process.",
+    "Free orgainsational material for running the sports day(measuring taps, balls, stopwatches, etc.)",
+    "You will receive bib numbers and participant certificates free of charge.",
+    "Data protection is guranteed at all times.",
+  ];
+
   return (
     <div
       className={
@@ -26,11 +35,17 @@ const ExpandableCard: React.FC<ExpandableCardProps> = ({
         >
           {isExpanded ? (
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-              <path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z" />
+              <path
+                d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
+                fill="white"
+              />
             </svg>
           ) : (
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-              <path d="M17 15.586 6.707 5.293 5.293 6.707 15.586 17H7v2h12V7h-2v8.586z" />
+              <path
+                d="M17 15.586 6.707 5.293 5.293 6.707 15.586 17H7v2h12V7h-2v8.586z"
+                fill="white"
+              />
             </svg>
           )}
         </button>
@@ -57,25 +72,18 @@ const ExpandableCard: React.FC<ExpandableCardProps> = ({
           {content}
           <br />
           <br />
-          <ul className="list-inside list-disc">
-            <li>
-              A ready-to-use sport's day concept (including various ways of
-              orgainising the event)
-            </li>
-            <li>
-              Simple evaluation software that enables you to capture results and
-              analyse them in ranking lists.
-            </li>
-            <li>Free result sheets to simplify the organisational process.</li>
-            <li>
-              Free orgainsational material for running the sports day(measuring
-              taps, balls, stopwatches, etc.)
-            </li>
-            <li>
-              You will receive bib numbers and participant certificates free of
-              charge.
-            </li>
-            <li>Data protection is guranteed at all times.</li>
+          <ul className="pl- list-none">
+            {bulletPoints.map((point, index) => (
+              <li key={index} className="relative mb-2 pl-6">
+                <span
+                  className="absolute left-0 top-0 text-2xl leading-none text-black"
+                  style={{ width: "0.5rem" }}
+                >
+                  •
+                </span>
+                {point}
+              </li>
+            ))}
           </ul>
         </div>
       ) : (
