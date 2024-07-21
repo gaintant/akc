87677819ddc26ee9dd376679325490ef930d4e3f
Siddhesh Kothadi <realsiddheshko@gmail.com>
2024-07-21 13:40:29 +0530

    Fix shadow for expandable card

--- src/app/(components)/ExpandableCard.tsx
+++ src/app/(components)/ExpandableCard.tsx
@@ -23,11 +23,7 @@ const ExpandableCard: React.FC<ExpandableCardProps> = ({
   return (
     <div
       onClick={onClick}
-      className={
-        isExpanded
-          ? `w-[550px] cursor-pointer relative mb-4 rounded-lg border border-black bg-white p-4 shadow-[0_4px_6px_rgba(0,0,0,0.8),0_1px_3px_rgba(0,0,0,0.08)]`
-          : `w-[550px] cursor-pointer relative mb-4 rounded-lg border border-black p-4`
-      }
+      className={`transition-all w-[550px] cursor-pointer relative mb-4 rounded-2xl border-[1.5px] border-gray-700 p-4 ${isExpanded && 'bg-white shadow-[2px_4px_0_#12141d]'}`}
     >
       <div className="flex items-center">
         <button
