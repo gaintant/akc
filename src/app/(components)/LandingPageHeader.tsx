70a5939b53cd2e3c434841f0ba34f59d0957d7cb
Atharva <atharvadere1018@gmail.com>
2024-07-30 15:00:58 +0530

    First section

--- src/app/(components)/LandingPageHeader.tsx
+++ src/app/(components)/LandingPageHeader.tsx
@@ -2,24 +2,24 @@ import Image from "next/image";
 
 const LandingPageHeader = () => {
   return (
-    <header className="content-container top-0 flex items-center justify-between bg-white py-3 px-4 md:p-4">
+    <header className="w-full lg:content-container top-0 flex items-center justify-between bg-white py-3 px-4 lg:p-4">
       <div className="flex items-center">
         <Image
           src="/images/AKCLogo_ALt.png"
           alt="Athletics Kids Cup Logo"
           height={71.16}
           width={360}
-          className="h-auto w-[212px] md:w-[360px]"
+          className="h-auto w-[212px] lg:w-[360px]"
         />
       </div>
-      <div className="flex flex-col md:flex-row md:items-center md:gap-4">
+      <div className="flex flex-col lg:flex-row lg:items-center lg:gap-4">
         <span className="text-[7px] sm:text-sm sm:font-medium">Enabled by</span>
         <Image
           src="/images/ubs-logo.png"
           alt="UBS Logo"
           height={54}
           width={146.12}
-          className="h-auto w-[74px] md:w-[146px]"
+          className="h-auto w-[74px] lg:w-[146px]"
         />
       </div>
     </header>
--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import React from "react";
+import PreRegistrationButtonWithModal from "../(components)/PreRegistrationButtonWithModal";
 
 const Mobile = () => {
   return (
-    <>
+    <div id="home-container">
       {/* <div className="container h-[700px] w-full">
         <Image
           src="/images/three-kids-hero.png"
@@ -14,21 +15,21 @@ const Mobile = () => {
           className="px-4"
         />
       </div> */}
-      <div className="flex w-full flex-1 flex-col justify-center gap-12 lg:w-1/2">
-        <p className="flex h-[32px] w-[168px] lg:w-[187px] items-center justify-center rounded-full bg-greyBg px-[12px] py-[6px] text-sm tracking-[2px]">
+      <div className="flex flex-col items-center justify-center gap-3 px-4 lg:w-1/2">
+        <p className="flex h-[32px] w-[168px] items-center justify-center rounded-full bg-greyBg px-[12px] py-[6px] text-sm tracking-[2px] lg:w-[187px]">
           #WE<span className="font-bold">MOVE</span>THE
           <span className="font-bold">KIDS</span>
         </p>
         <div className="flex w-max flex-col">
           <div className="flex gap-4">
-            <h1 className="text-5xl font-semibold">Running</h1>
+            <h1 className="text-4xl font-semibold lg:text-5xl">Running</h1>
             <Image
               src="/images/svgs/rjt-orange.svg"
               width={35.4}
               height={33.52}
               alt="orange icon"
             />
-            <h1 className="text-5xl font-semibold">Jumping</h1>
+            <h1 className="text-4xl font-semibold lg:text-5xl">Jumping</h1>
           </div>
           <div className="flex justify-center gap-4">
             <Image
@@ -37,7 +38,7 @@ const Mobile = () => {
               height={28.71}
               alt="black icon"
             />
-            <h1 className="text-5xl font-semibold">Throwing</h1>
+            <h1 className="text-4xl font-semibold lg:text-5xl">Throwing</h1>
             <Image
               src="/images/svgs/rjt-right.svg"
               width={29.45}
@@ -46,14 +47,15 @@ const Mobile = () => {
             />
           </div>
         </div>
-        <p className="max-w-[486px] text-base font-medium leading-7 tracking-[-0.01em]">
+        <p className="max-w-[486px] text-center text-base font-medium leading-7 tracking-[-0.01em] lg:text-left">
           The Athletics Kids Cup moves the kids! The three basic movement forms
           that are required in almost every sport are the basis of this unique
           sports initiative in India. Let&apos;s move the children together and
           encourage their personal development.
         </p>
+        <PreRegistrationButtonWithModal />
       </div>
-    </>
+    </div>
   );
 };
 
