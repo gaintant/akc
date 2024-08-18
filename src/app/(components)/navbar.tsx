c7659e6fd089d6ef4443e47b8c9576594ddaf708
prashantbariya208 <prashantbariya208@gmail.com>
2024-08-18 20:23:11 +0530

    Added SubItems

--- src/app/(components)/navbar.tsx
+++ src/app/(components)/navbar.tsx
@@ -2,60 +2,35 @@
 
 import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import { SlMenu, SlArrowDown, SlArrowUp, SlArrowRight } from 'react-icons/sl';
 import clsx from 'clsx';
+import NavButton from './NavButton';
+import { SlMenu, SlArrowDown, SlArrowUp, SlArrowRight } from 'react-icons/sl';
 import { RxCross2 } from "react-icons/rx";
 import Image from "next/image";
 
 const navItems = [
-  {
-    href: '/',
-    label: 'Home',
-    subItems: [
-      { href: '/home/sub1', label: 'Sub Home 1' },
-      { href: '/home/sub2', label: 'Sub Home 2' },
-    ],
-  },
+  { href: '/', label: 'Home' },
   {
     href: '/competition',
     label: 'Competition',
     subItems: [
-      { href: '/competition/sub1', label: 'Sub Competition 1' },
-      { href: '/competition/sub2', label: 'Sub Competition 2' },
+      { href: '/howItWorks', label: 'How it works' },
+      { href: '/participation', label: 'Who can participate?' },
+      { href: '/disciplines', label: 'The three disciplines' },
+      { href: '/rulesAndCategories', label: 'Rules and categories' },
+      { href: '/finals', label: 'Finals' },
     ],
   },
   {
     href: '/organizers',
     label: 'Organizers',
     subItems: [
-      { href: '/organizers/sub1', label: 'Sub Organizers 1' },
-      { href: '/organizers/sub2', label: 'Sub Organizers 2' },
+      { href: '/becomeAnOrganizer', label: 'Become an organizer' },
+      { href: '/documentsAndDownloads', label: 'Documents and downloads' },
     ],
   },
-  {
-    href: '/faq',
-    label: 'FAQ',
-    subItems: [
-      { href: '/faq/sub1', label: 'Sub FAQ 1' },
-      { href: '/faq/sub2', label: 'Sub FAQ 2' },
-    ],
-  },
-  {
-    href: '/about',
-    label: 'About AKC',
-    subItems: [
-      { href: '/about/sub1', label: 'Sub About 1' },
-      { href: '/about/sub2', label: 'Sub About 2' },
-    ],
-  },
-  {
-    href: '/contact',
-    label: 'Contact Us',
-    subItems: [
-      { href: '/contact/sub1', label: 'Sub Contact 1' },
-      { href: '/contact/sub2', label: 'Sub Contact 2' },
-    ],
-  },
+  { href: '/faq', label: 'FAQ' },
+  { href: '#contact-us', label: 'Contact Us' },
 ];
 
 const Navbar = () => {
@@ -74,7 +49,6 @@ const Navbar = () => {
     setOpenSubMenu(null);
   };
 
-  // Close the menu when clicking outside of it
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
@@ -95,7 +69,6 @@ const Navbar = () => {
 
   return (
     <div>
-      {/* Navbar Toggle Button */}
       <button
         onClick={toggleMenu}
         className="p-2 text-gray-600 focus:outline-none fixed bottom-4 right-4 md:static md:bottom-auto md:right-auto z-20"
@@ -103,16 +76,21 @@ const Navbar = () => {
         <SlMenu fontSize={40} />
       </button>
 
-      {/* Sidebar Menu */}
+      <NavButton
+        onClick={toggleMenu}
+        className="fixed bottom-4 right-4 md:hidden z-20"
+        aria-label="Toggle Navbar"
+      />
+
       <div
         ref={menuRef}
-        className={`fixed top-0 right-0 w-3/4 md:w-1/4 h-full bg-white shadow-lg z-10 transform ${
+        className={`fixed top-0 right-0 w-full md:w-1/3 h-full bg-white shadow-lg z-10 transform ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-300`}
       >
         <div className="flex p-2 justify-between">
           <Link href="/">
-            <div className="flex items-center w-2/3 ">
+            <div className="flex items-center w-2/3">
               <Image
                 src="/images/AKCLogo_ALt.png"
                 alt="Athletics Kids Cup Logo"
@@ -139,61 +117,98 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* Links */}
         <nav className="mt-8">
           <ul className="space-y-6">
             {navItems.map(({ href, label, subItems }) => (
               <li key={href} className="px-6">
-                <div
-                  className="flex items-center justify-between text-gray-700 text-xl hover:text-red-600 cursor-pointer"
-                  onClick={() => toggleSubMenu(label)}
-                >
-                  <div
-                    className="flex flex-row items-center space-x-2"
-                    onMouseEnter={() => setHoveredItem(label)}
-                    onMouseLeave={() => setHoveredItem(null)}
-                  >
-                    <button
-                      className={clsx(
-                        'flex h-8 w-8 min-w-8 transform items-center justify-center rounded-full transition-transform duration-200',
-                        !hoveredItem || hoveredItem !== label
-                          ? 'bg-white text-black shadow-[2px_3px_0_#12141d]'
-                          : 'bg-green-300 text-black shadow-[2px_3px_0_#12141d]'
-                      )}
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                        <path
-                          d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
-                          fill="black"
-                          transform={hoveredItem === label ? "rotate(135 12 12)" : "rotate(90 12 12)"}
-                        />
-                      </svg>
-                    </button>
+                {!subItems ? (
+                  <Link href={href}>
                     <div
-                      className={clsx(
-                        !hoveredItem || hoveredItem !== label
-                          ? 'text-black'
-                          : 'text-green-300 font-bold'
-                      )}
+                      className="flex flex-row items-center space-x-2 text-gray-700 text-xl hover:text-red-600 cursor-pointer"
+                      onMouseEnter={() => setHoveredItem(label)}
+                      onMouseLeave={() => setHoveredItem(null)}
                     >
-                      {label}
+                      <button
+                        className={clsx(
+                          'flex h-8 w-8 min-w-8 transform items-center justify-center rounded-full transition-transform duration-200',
+                          !hoveredItem || hoveredItem !== label
+                            ? 'bg-white text-black shadow-[2px_3px_0_#12141d]'
+                            : 'bg-green-300 text-black shadow-[2px_3px_0_#12141d]'
+                        )}
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                          <path
+                            d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
+                            fill="black"
+                            transform={hoveredItem === label ? "rotate(135 12 12)" : "rotate(90 12 12)"}
+                          />
+                        </svg>
+                      </button>
+                      <div
+                        className={clsx(
+                          !hoveredItem || hoveredItem !== label
+                            ? 'text-black'
+                            : 'text-green-300 font-bold'
+                        )}
+                      >
+                        {label}
+                      </div>
                     </div>
-                  </div>
-                  <span className="ml-auto">
-                    {openSubMenu === label ? <SlArrowUp /> : <SlArrowDown />}
-                  </span>
-                </div>
-                {openSubMenu === label && (
-                  <ul className="pl-10 mt-2 space-y-2 transition-all duration-300">
-                    {subItems.map((subItem) => (
-                      <li key={subItem.href}>
-                        <Link href={subItem.href} className="text-gray-600 text-lg hover:text-red-500 flex items-center">
-                          <SlArrowRight className="mr-2" /> {/* Horizontal arrow icon */}
-                          {subItem.label}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                  </Link>
+                ) : (
+                  <>
+                    <div
+                      className="flex items-center justify-between text-gray-700 text-xl hover:text-red-600 cursor-pointer"
+                      onClick={() => toggleSubMenu(label)}
+                    >
+                      <div
+                        className="flex flex-row items-center space-x-2"
+                        onMouseEnter={() => setHoveredItem(label)}
+                        onMouseLeave={() => setHoveredItem(null)}
+                      >
+                        <button
+                          className={clsx(
+                            'flex h-8 w-8 min-w-8 transform items-center justify-center rounded-full transition-transform duration-200',
+                            !hoveredItem || hoveredItem !== label
+                              ? 'bg-white text-black shadow-[2px_3px_0_#12141d]'
+                              : 'bg-green-300 text-black shadow-[2px_3px_0_#12141d]'
+                          )}
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                            <path
+                              d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
+                              fill="black"
+                              transform={hoveredItem === label ? "rotate(135 12 12)" : "rotate(90 12 12)"}
+                            />
+                          </svg>
+                        </button>
+                        <div
+                          className={clsx(
+                            !hoveredItem || hoveredItem !== label
+                              ? 'text-black'
+                              : 'text-green-300 font-bold'
+                          )}
+                        >
+                          {label}
+                        </div>
+                      </div>
+                      <span className="ml-auto">
+                        {openSubMenu === label ? <SlArrowUp /> : <SlArrowDown />}
+                      </span>
+                    </div>
+                    {openSubMenu === label && subItems && (
+                      <ul className="pl-10 mt-2 space-y-2 transition-all duration-300">
+                        {subItems.map((subItem) => (
+                          <li key={subItem.href}>
+                            <Link href={subItem.href} className="text-gray-600 text-lg hover:text-red-500 flex items-center">
+                              <SlArrowRight className="mr-2" />
+                              {subItem.label}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </>
                 )}
                 <hr className="mt-2 border-dashed border-gray-300" />
               </li>
