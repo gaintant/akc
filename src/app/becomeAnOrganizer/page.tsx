77cd62571683f69326392e910267754a0b5e642a
prashantbariya208 <prashantbariya208@gmail.com>
2024-08-19 21:41:19 +0530

    Added component for BecomeAnOrganizers

--- src/app/becomeAnOrganizer/page.tsx
+++ src/app/becomeAnOrganizer/page.tsx
@@ -9,64 +9,69 @@ import Arrow from "../(components)/Icons/Arrow";
 import PreRegistrationForm from "../(components)/PreRegistrationForm";
 import OrganisationalMaterial from "../(components)/becomeanorganiser3";
 import Carousel from "./carousel";
+
 export default function NewHowItWorks() {
   const [expandedCard, setExpandedCard] = useState<number | null>(1);
+
   return (
     <section
       id="how-it-works"
       className="flex min-h-screen w-full flex-col items-center justify-between bg-white p-4"
     >
-      {/* First component*/}
+      {/* First component */}
       <div className="relative mt-24 w-full">
-  <div className="container mx-auto flex flex-col-reverse p-4 md:flex-row">
-    <div className="order-2 mb-4 w-full min-w-[300px] md:order-1 md:mb-0 md:ml-24 md:w-3/4 md:px-12">
-      <div className="md:mt-8">
-        <div className="md:mt-12">
-          <h2 className="text-[2.5rem] font-semibold leading-[48px] tracking-[-0.04em]">
-            How easy it is to organise an Athletics Kids Cup 
-          </h2>
-          <div>
+        <div className="container mx-auto flex flex-col-reverse p-4 md:flex-row">
+          <div className="order-2 mb-4 w-full min-w-[300px] md:order-1 md:mb-0 md:ml-24 md:w-3/4 md:px-12">
+            <div className="md:mt-8">
+              <div className="md:mt-12">
+                <h2 className="text-3xl md:text-[2.5rem] font-semibold leading-tight md:leading-[48px] tracking-[-0.04em]">
+                  How easy it is to organise an Athletics Kids Cup
+                </h2>
+                <div className="mt-4 md:mt-6">
+                  <Image
+                    src="images/becomeanorganizer/redzigzag.svg"
+                    width={325}
+                    height={15.39}
+                    alt="red zigzag line"
+                  />
+                </div>
+              </div>
+              <p className="mt-6 text-base md:text-lg">
+                We offer an unbeatably attractive package for all schools that
+                want to get their students moving. Through the Athletics Kids
+                Cup, we provide the ideal platform for children to build and
+                strengthen their character, values, integrity, mutual respect,
+                and personal discipline. We inspire them to get the best out of
+                themselves and to pursue their own ambitious goals.
+              </p>
+              <div className="py-6 md:py-12">
+                <PreRegistrationForm />
+              </div>
+            </div>
+          </div>
+          <div className="relative order-1 mx-auto mb-4 w-full sm:h-[auto] md:order-2 md:mb-0">
             <Image
-              src="images/becomeanorganizer/redzigzag.svg"
-              width={325}
-              height={15.39}
-              alt="black icon"
+              src="/images/becomeanorganizer/become-an-organizer-1.png"
+              alt="Become an Organizer"
+              width={600}
+              height={610}
+              className="object-cover"
             />
           </div>
         </div>
-        <p className="mt-6 text-lg">
-          We offer an unbeatably attractive package for all schools that want to get their students moving. 
-          Through the Athletics Kids Cup, we provide the ideal platform for children to build and strengthen 
-          their character, values, integrity, mutual respect and personal discipline. 
-          We inspire them to get the best out of themselves and to pursue their own ambitious goals.
-        </p>
-        <div className="py-10 md:py-12">
-          <PreRegistrationForm />
-        </div>
       </div>
-    </div>
-    <div className="relative order-1 mx-auto mb-4 h-auto w-full sm:h-[auto] md:order-2 md:mb-0">
-      <Image
-        src="/images/becomeanorganizer/become-an-organizer-1.png"
-        alt="Image"
-        width={600}
-        height={610}
-        className="object-cover"
-      />
-    </div>
-  </div>
-</div>
 
       <DesignBar />
-      <Carousel />
-      <OrganisationalMaterial/>
-      {/* Third component*/}
-      <div>
-        <div className="mt-12">
+      {/* <Carousel /> */}
+      <OrganisationalMaterial />
+
+      {/* Third component */}
+      <div className="w-full">
+        <div className="mt-12 flex justify-center">
           <img
             src="/images/how-it-works/the-competition.png"
             alt="The Competition"
-            className="w-[15.625rem] rounded-md object-cover"
+            className="w-3/4 md:w-[15.625rem] rounded-md object-cover"
           />
         </div>
         <div className="mx-auto my-8 max-w-6xl p-4">
@@ -76,30 +81,30 @@ export default function NewHowItWorks() {
               {/* Left Side - Image */}
               <div className="w-full sm:w-2/5">
                 <Image
-                  src="/images/how-it-works/competition-card-1.png" // Replace with your image path
+                  src="/images/how-it-works/competition-card-1.png"
                   alt="Card Image 1"
                   layout="responsive"
-                  width={400} // Adjust based on your image size
+                  width={400}
                   height={476}
                   className="rounded-lg object-contain"
                 />
               </div>
 
               {/* Right Side - Content */}
-              <div className="flex w-full flex-col justify-between px-8 py-4 sm:w-3/4">
+              <div className="flex w-full flex-col justify-between px-4 py-4 md:px-8 sm:w-3/4">
                 <div>
-                  <h2 className="mb-2 text-2xl font-bold">
+                  <h2 className="mb-2 text-xl md:text-2xl font-bold">
                     Categories and rules
                   </h2>
-                  <p className="mb-2 font-semibold">
-                    Here you can find out everything about the categories,
-                    rules, scoring and conditions of participation.
+                  <p className="mb-2 text-sm md:text-base font-semibold">
+                    Here you can find out everything about the categories, rules,
+                    scoring, and conditions of participation.
                   </p>
 
                   <Link href="/rulesAndCategories" passHref>
                     <Button
                       variant={ButtonVariant.GREEN}
-                      className="flex items-center gap-3"
+                      className="flex items-center gap-3 mt-4"
                     >
                       <span>More Info</span>
                       <Arrow className="w-5 -rotate-45" fill="#374151" />
@@ -114,22 +119,22 @@ export default function NewHowItWorks() {
               {/* Left Side - Image */}
               <div className="w-full sm:w-2/5">
                 <Image
-                  src="/images/how-it-works/competition-card-2.png" // Replace with your image path
+                  src="/images/how-it-works/competition-card-2.png"
                   alt="Card Image 2"
                   layout="responsive"
-                  width={400} // Adjust based on your image size
+                  width={400}
                   height={476}
                   className="rounded-lg object-contain"
                 />
               </div>
 
               {/* Right Side - Content */}
-              <div className="flex w-full flex-col justify-between px-8 py-4 sm:w-3/4">
+              <div className="flex w-full flex-col justify-between px-4 py-4 md:px-8 sm:w-3/4">
                 <div>
-                  <h2 className="mb-2 text-2xl font-bold">
+                  <h2 className="mb-2 text-xl md:text-2xl font-bold">
                     The three disciplines
                   </h2>
-                  <p className="mb-2 font-semibold">
+                  <p className="mb-2 text-sm md:text-base font-semibold">
                     Running - Jumping - Throwing: All about the disciplines in
                     the Athletics Kids Cup.
                   </p>
@@ -137,7 +142,7 @@ export default function NewHowItWorks() {
                   <Link href="/disciplines" passHref>
                     <Button
                       variant={ButtonVariant.CYAN}
-                      className="flex items-center gap-3"
+                      className="flex items-center gap-3 mt-4"
                     >
                       <span>More Info</span>
                       <Arrow className="w-5 -rotate-45" fill="#374151" />
@@ -149,8 +154,6 @@ export default function NewHowItWorks() {
           </div>
         </div>
       </div>
-
     </section>
-   
   );
 }
